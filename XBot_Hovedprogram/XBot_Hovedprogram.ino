14ae9d1fe4f58fab5fffe5fb48b39b9c5c57a8c1
dammen <jonasdam@stud.ntnu.no>
2015-03-25 20:03:52 +0100

    Update XBot_Hovedprogram.ino
    lagt inn tidsvariabler som definerer når hovedboten skal til en post. 
    Lagt til sensorP(1,2,3,4,5) - disse skal bli "true" når boten registrerer riktig grå-nyanse.

--- XBot_Hovedprogram/XBot_Hovedprogram.ino
+++ XBot_Hovedprogram/XBot_Hovedprogram.ino
@@ -8,6 +8,20 @@ boolean post3 = false;
 boolean post4 = false;
 boolean post5 = false; // Kosepost, skal brukes til touch-kos.
 
+boolean sensorP1 = false;
+boolean sensorP2 = false;
+boolean sensorP3 = false;
+boolean sensorP4 = false;
+boolean sensorP5 = false;
+
+long time= millis();
+long timePost1 = millis();
+long timePost2 = millis();
+long timePost3 = millis();
+long timePost4 = millis();
+long timePost5 = millis();
+
+
 /*
 Sett inn initialverdier til post1
 */
@@ -20,7 +34,6 @@ Sett inn initialverdier til post2
 // til Post 3
 boolean happy3;
 int count = 0;
-int time = 0;
 int lys = 0;
 const int ledPin1 = 12;
 const int ledPin2 = 11;
@@ -39,6 +52,8 @@ Sett inn initialverdier til post4
 
 
 void setup(){
+ 
+
   /*
   sett inn setup til post1
   */
@@ -69,39 +84,95 @@ void setup(){
 
 void loop(){
   /*sett inn kode for når de forskjellige tilstandene skal være true og false
-  ...
   */
+  time = millis();
+  
+  if (time - timePost1 > 10000){
+     post1 = true;
+  }
+  if (time - timePost2 > 20000){
+     post2 = true;
+  }
+  if (time - timePost3 > 30000){
+     post3 = true;
+  }
+  if (time - timePost4 > 40000){
+     post4 = true;
+  }
+  if (time - timePost5 > 50000){
+     post5 = true;
+  }
+  
+  
   
   /* Sett inn kode for å finne ut hvilken stasjon/post den er på
   ...
   */
   
-  if (post1==true){
+  if (post1==true && sensorP1 == true){
     /* sett inn kode for oppførsel på post1
     ...
     */
+    
+    //dette er det siste som gjøres på posten
+    post1 = false;
+    timePost1 = millis();
+    
   }  
-  if (post2 == true){
+  if (post2 == true && sensorP2 == true){
     /* sett inn kode for oppførsel på post2
     ...
     */
+    
+     //dette er det siste som gjøres på posten
+    post2 = false;
+    timePost2 = millis();
   }
-  if (post3 == true){
+  if (post3 == true && sensorP3 == true){
     /* sett inn kode for oppførsel på post3
     ...
     */
+    
+     //dette er det siste som gjøres på posten
+    post3 = false;
+    timePost3 = millis();
   }
-  if (post4 == true){
+  if (post4 == true && sensorP4 == true){
     /* sett inn kode for oppførsel på post4
     ...
     */
+    
+     //dette er det siste som gjøres på posten
+    post4 = false;
+    timePost4 = millis();
   }
-  if (post5 == true){
+  if (post5 == true && sensorP5 == true){
     /* sett inn kode for oppførsel på post5
     ...
     */
+    
+     //dette er det siste som gjøres på posten
+    post5 = false;
+    timePost5 = millis();
   }
+  
+  Serial.println(time);
+  Serial.println("Post1: "); 
+  Serial.println(timePost1);
+  Serial.println("Post2: "); 
+  Serial.println(timePost2);
+  Serial.println("Post3: "); 
+  Serial.println(timePost3);
+  Serial.println("Post4: "); 
+  Serial.println(timePost4);
+  Serial.println("Post5: "); 
+  Serial.println(timePost5);
+  Serial.println(" ");
+  
+  delay(1000);
 }
+  
+  
   
   
   
