5070544cbf613f1cbb1cf2b297800f748740777b
dammen <jonasdam@stud.ntnu.no>
2015-04-08 19:48:32 +0200

    Update XBot_Hovedprogram.ino

--- XBot_Hovedprogram/XBot_Hovedprogram.ino
+++ XBot_Hovedprogram/XBot_Hovedprogram.ino
@@ -14,12 +14,27 @@ boolean sensorP3 = false;
 boolean sensorP4 = false;
 boolean sensorP5 = false;
 
+boolean soundP1 = false;
+boolean soundP2 = false;
+boolean soundP3 = false;
+boolean soundP4 = false;
+boolean soundP5 = false;
+
 long time= millis();
 long timePost1 = millis();
 long timePost2 = millis();
 long timePost3 = millis();
 long timePost4 = millis();
 long timePost5 = millis();
+
+// til lydsensor paa Hovedbot
+int micValue;
+int micInput = A0;
+int toneLength;
+int count;
+int waitingTime;
+int timeWaited = 0;
+
 
 
 /*
@@ -33,13 +48,11 @@ Sett inn initialverdier til post2
 
 // til Post 3
 boolean happy3;
-int count = 0;
-int lys = 0;
+int countP3 = 0;
+int lysP3 = 0;
 const int ledPin1 = 12;
 const int ledPin2 = 11;
 int speakerOut = 9;
-int micInput = A0;
-int micValue = 0;
 int frequency = 880;
 
 /*
@@ -53,7 +66,12 @@ Sett inn initialverdier til post4
 
 void setup(){
  
-
+  // setup til lydsensor hovedbot
+  pinMode(micInput, INPUT);
+  Serial.begin(9600);
+  waitingTime = 10000;
+  micValue = 0;
+  
   /*
   sett inn setup til post1
   */
@@ -105,9 +123,45 @@ void loop(){
   
   
   
+ 
+  
   /* Sett inn kode for å finne ut hvilken stasjon/post den er på
-  ...
+  Her registrerer  mikrofonen en lyd. hvis den registrerer flere lyder innen den 10 sekunder, saa oeker counteren.
   */
+  micValue = analogRead(micInput);
+  if (micValue > 500){
+    count +=1;
+    timeWaited = 0;
+    while(timeWaited < waitingTime){
+      micValue = analogRead(micInput);
+      if (micValue > 500){
+        count +=1;
+      }
+      timeWaited += 100;
+      delay(50);
+    }
+    timeWaited = 0;
+  }
+  
+  
+  // her sjekker den hva counteren er og setter tilsvarende SoundPost til true
+  if(count>2 && count < 20){
+    soundP1 = true;
+  }
+  if(count>20 && count < 40){
+    soundP2 = true;
+  }
+  if(count>40 && count < 60){
+    soundP3 = true;
+  }
+  if(count>60 && count < 80){
+    soundP4 = true;
+  }
+  if(count>80 && count < 100){
+    soundP5 = true;
+  }
+  count = 0;
+  
   
   if (post1==true && sensorP1 == true){
     /* sett inn kode for oppførsel på post1
@@ -117,6 +171,7 @@ void loop(){
     //dette er det siste som gjøres på posten
     post1 = false;
     timePost1 = millis();
+    soundP1 = false;
     hunger4++;
   }  
   if (post2 == true && sensorP2 == true){
@@ -127,6 +182,7 @@ void loop(){
      //dette er det siste som gjøres på posten
     post2 = false;
     timePost2 = millis();
+    soundP2= false;
     hunger4++;
   }
   if (post3 == true && sensorP3 == true){
@@ -137,6 +193,7 @@ void loop(){
      //dette er det siste som gjøres på posten
     post3 = false;
     timePost3 = millis();
+    soundP3= false;
     hunger4++;
   }
   if (post4 == true && sensorP4 == true){
@@ -146,6 +203,7 @@ void loop(){
     delay(500);
     post4 = false;
     timePost4 = millis();
+    soundP4 = false;
   }
   if (post5 == true && sensorP5 == true){
     /* sett inn kode for oppførsel på post5
@@ -155,9 +213,11 @@ void loop(){
      //dette er det siste som gjøres på posten
     post5 = false;
     timePost5 = millis();
+    soundP5 = false;
     hunger++;
   }
   
+  /* Dette kan tas med for å teste at tidsvariablene fungerer
   Serial.println(time);
   Serial.println("Post1: "); 
   Serial.println(timePost1);
@@ -170,8 +230,8 @@ void loop(){
   Serial.println("Post5: "); 
   Serial.println(timePost5);
   Serial.println(" ");
-  
   delay(1000);
+  */
 }
   
   
